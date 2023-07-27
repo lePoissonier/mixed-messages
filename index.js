3b2af5a3e87f180fad3d210034a49333e27d30ab
alain eloma <alainelomapro@gmail.com>
2023-07-27 10:58:24 +0100

    declared quotes list

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // quotes
 
-const quotes = []
+const quotes = ["Only I Can Call My Dream Stupid! - Roronoa Zoro",
+"People’s Dreams... Have No Ends! - Marshall D. Teach",
+"Justice Will Prevail, You Say? But Of Course, It Will! Whoever Wins This War Becomes Justice! - Donquixote Doflamingo",
+"You Want To Keep Everyone From Dying? That’s Naive. It’s War — People Die",
+"His Power Is To Rally Those Around Him. Of All The People Seeking To Conquer The Sea, He May Possess The Greatest Talent Of All. - Dracule Mihawk"
+]
