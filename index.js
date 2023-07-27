17307eb0dbaef649547eab23846a068f17929650
alain eloma <alainelomapro@gmail.com>
2023-07-27 11:42:25 +0100

    function commentary

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const quotes = ["Only I Can Call My Dream Stupid! - Roronoa Zoro",
 "His Power Is To Rally Those Around Him. Of All The People Seeking To Conquer The Sea, He May Possess The Greatest Talent Of All. - Dracule Mihawk"
 ]
 
+// function to show random quotes 
+
 function randomOnePiceQuote () {
     let randomNumber = Math.floor(Math.random() * (quotes.length));
     console.log(quotes[randomNumber]);
