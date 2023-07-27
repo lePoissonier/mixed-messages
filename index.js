94a63b537ffb40d33b8bac4cdf53439527f1b27d
alain eloma <alainelomapro@gmail.com>
2023-07-27 11:14:30 +0100

    i created the randomOnePiceQuote function

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const quotes = ["Only I Can Call My Dream Stupid! - Roronoa Zoro",
 "You Want To Keep Everyone From Dying? That’s Naive. It’s War — People Die",
 "His Power Is To Rally Those Around Him. Of All The People Seeking To Conquer The Sea, He May Possess The Greatest Talent Of All. - Dracule Mihawk"
 ]
+
+function randomOnePiceQuote () {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    console.log(quotes[randomNumber]);
+}
+
+randomOnePieceQuote();
